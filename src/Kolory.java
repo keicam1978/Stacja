1de04ee08b2ce8495eae677174ae1baab9d4774d
Artur Ciesla <ezechiasz.ofm@gmail.com>
2018-12-10 19:36:13 +0100

    0.02

--- src/Kolory.java
+++ src/Kolory.java
@@ -1,5 +1,6 @@
 public class Kolory {
 
-    public enum kolor {czerwony, zielony, zolty}
+    public enum kolor {czerwony, zielony, zolty, brak}
+    public enum predkosc {stop, p40, p60, p100, p160, max}
 
 }
--- src/Semafor.java
+++ src/Semafor.java
@@ -1,16 +1,33 @@
 
 
 public class Semafor {
-    Kolory.kolor upLight;
-    Kolory.kolor stop;
-    Kolory.kolor downLigh;
-    Kolory.kolor belt;
+
+
+
+
+    private Kolory.kolor upLight;
+    private Kolory.kolor stop;
+    private Kolory.kolor downLigh;
+    private Kolory.kolor belt;
     boolean N;
+
+
+    public Semafor(Kolory.kolor gorne, Kolory.kolor dolne, Kolory.kolor pas, boolean N){
+        upLight=gorne;
+        downLigh=dolne;
+        belt=pas;
+        this.N=N;
+
+    }
 
     private  void setLight(){
 
     }
 
-    private
+    private Kolory.predkosc getSpeed(Kolory.kolor gorne, Kolory.kolor dolne, Kolory.kolor pas){
+        Kolory.predkosc V= Kolory.predkosc.stop;
+        
+        return V;
+    }
 
 }
